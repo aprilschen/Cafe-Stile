fcb0a5c53db7c2adffcc44815d042cfdf81ebebb
Shiwei Chen <april.chen.0615@gmail.com>
2023-02-17 11:20:57 -0800

    Added Listing Model

--- server/backend/services/models.py
+++ server/backend/services/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Listing(models.Model):
+    locationImages = ArrayField(models.CharField(max_length=3000, blank=True))
+    location = models.CharField(max_length=120)
+    days = models.CharField(max_length=120)
+    price = models.CharField(max_length=120)
+    isNew = models.BooleanField(default=True)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+
+    def __str__(self):
+        return str(self.location)
