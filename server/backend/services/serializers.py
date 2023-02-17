eab88ffeaa5d8a48f7cb735d4fb2c16c509a2f14
Shiwei Chen <april.chen.0615@gmail.com>
2023-02-17 11:40:27 -0800

    app scaffolding & connections

---
+++ server/backend/services/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Listing
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = '__all__'
--- server/backend/services/views.py
+++ server/backend/services/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from .models import Listing
+from .serializers import ListingSerializer
 # Create your views here.
+def Listings(request):
+    if request.method=='GET':
+        queryset = Listing.objects.all()
+        data = ListingSerializer(queryset, many=True)
+        return Response ({"data": data.data})
