be1f595e4c9de466354d105fb7a3eae40fff68d3
Shiwei Chen <april.chen.0615@gmail.com>
2023-02-17 18:52:36 -0800

    Implemented post requests to Server

--- server/backend/services/views.py
+++ server/backend/services/views.py
@@ -4,9 +4,15 @@ from .models import Listing
 from .serializers import ListingSerializer
 from rest_framework.decorators import api_view
 # Create your views here.
-@api_view(['GET',])
+@api_view(['GET', 'POST'])
 def Listings(request):
     if request.method=='GET':
         queryset = Listing.objects.all()
         data = ListingSerializer(queryset, many=True)
         return Response ({"data": data.data})
+
+    if request.method=='POST':
+        listing = ListingSerializer(data = request.data)
+        listing.is_valid(raise_exception=True)
+        listing.save()
+        return Response({"Message": "Successfully created"}, 201)
--- src/components/ModalHostForm.js
+++ src/components/ModalHostForm.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
@@ -6,6 +5,10 @@ import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { TextField } from '@mui/material';
+import { useState } from 'react';
+
+import axios from 'axios';
+import qs from 'qs'
 
 const style = {
   position: 'absolute',
@@ -20,9 +23,34 @@ const style = {
 };
 
 export default function TransitionsModal() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+  const [days, setDays] = useState("");
+  const [price, setPrice] = useState("");
+  const [contact, setContact] = useState("");
+  const [tags, setTags] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const info = {
+      locationImageList: [],
+      title: title,
+      location: location,
+      description: description,
+      days: days,
+      price: price,
+      isNew: true,
+      contact: contact,
+      tags: tags,
+    }
+
+    axios.post("http://127.0.0.1:8000/api/listings", qs.stringify(info))
+  }
 
   return (
     <div>
@@ -43,7 +71,7 @@ export default function TransitionsModal() {
             <Typography variant="h6" component="h2">
               Host your Services
             </Typography>
-            <form>
+            <form onSubmit = {handleSubmit}>
                 <TextField
                 sx={{mb: 3}}
                 required
@@ -52,6 +80,7 @@ export default function TransitionsModal() {
                 type="text"
                 placeholder="Chef for hire!"
                 variant="filled"
+                onChange={e => setTitle(e.target.value)}
                 inputProps={{minLength: 2, maxLength: 120, style: {width: 300}}}
               />
 
@@ -63,6 +92,7 @@ export default function TransitionsModal() {
                 type="text"
                 placeholder="Cupertino, California"
                 variant="filled"
+                onChange={e => setLocation(e.target.value)}
                 inputProps={{minLength: 2, maxLength: 120, style: {width: 300}}}
               />
 
@@ -77,6 +107,7 @@ export default function TransitionsModal() {
                 type="text"
                 placeholder="Local dog walking services in the city of Cupertino."
                 variant="filled"
+                onChange={e => setDescription(e.target.value)}
                 inputProps={{maxLength: 2000, style: {width: 300}}}
               />
 
@@ -91,6 +122,7 @@ export default function TransitionsModal() {
               placeholder="Oct 2-9"
               variant="filled"
               helperText="Click here for help"
+              onChange={e => setDays(e.target.value)}
               inputProps={{maxLength: 200, style: {width: 300}}}
               />
 
@@ -104,6 +136,7 @@ export default function TransitionsModal() {
               type="text"
               placeholder="$20.00 per hour"
               variant="filled"
+              onChange={e => setPrice(e.target.value)}
               inputProps={{maxLength: 200, style: {width: 300}}}
 
               />
@@ -120,6 +153,7 @@ export default function TransitionsModal() {
               type="text"
               placeholder="How can potential clients contact you?"
               variant="filled"
+              onChange={e => setContact(e.target.value)}
               inputProps={{maxLength: 1000, style: {width: 300}}}
 
               />
@@ -136,6 +170,7 @@ export default function TransitionsModal() {
                 placeholder="Gig, Transportation, Tutoring"
                 variant="filled"
                 size="small"
+                onChange={e => setTags(e.target.value)}
                 inputProps={{maxLength: 2000, style: {width: 300}}}
               />
 
